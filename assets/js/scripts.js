0c97d130f699c22eb147f44895420f2ce4742e97
Josh Johnson <josh@jshjohnson.com>
2013-10-09 18:20:48 +0100

    Added Mixd CSS Framework

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+// @codekit-prepend "plugins.js"
+
+// Document Ready
+$(document).ready(function() {
+	// HTML5 placeholder support
+	$("input, textarea").placeholder();
+	// Flexible Textareas
+	$("textarea").not(".noresize, .noresize textarea").expandingTextarea();
+	// Target radios / checkboxes
+	$("input[type=radio]").parents('li').addClass('radio');
+	$("input[type=checkbox]").parents('li').addClass('checkbox');
+	// FitVids
+	$(".container").fitVids();
+	// Tweak required labels (Gravity Forms)
+	$(".gform_wrapper .gfield_required").html("(required)");
+	// Sanitise WP content
+	$("p:empty").remove();
+	$(".wp-caption").removeAttr("style");
+	$(".wp-content img, .wp-post-image, .wp-post-thumb").removeAttr("width").removeAttr("height");});
+
+// Window Load
+$(window).bind("load", function() {
+	// Fade container on load to combat FOUT
+	$(".container").animate({ opacity: 1 }, 'slow');
+});
