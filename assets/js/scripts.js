a7667f25b19a655ee7a1b47d00427f02d8b2d599
Josh Johnson <josh@jshjohnson.com>
2013-11-09 19:23:02 +0000

    Employer page job list

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 	    "<form action=''>" +
 	    "<textarea name='message' id='' cols='30' rows='15' placeholder='Write anything here that you think the freelancer will need to know about your project. The more detailed, the better!'></textarea>" +
 	    "<div class='button-container'>" +
-	    "<input class='submit' type='submit' value='Apply for your place'>" +
+	    "<input class='submit' type='submit' value='Send'>" +
 	    "</div>" +
 	    "</form>" +
 	    "</div>" +
