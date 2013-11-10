a1cb82abf059e3016be1864c79acc80c02eb1a12
Josh Johnson <josh@jshjohnson.com>
2013-11-10 10:51:19 +0000

    Safari browser fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,25 +19,13 @@ $(document).ready(function() {
 
 		var docHeight = $(document).height();
 
-	   $("body").append("" +
-	    "<section class='overlay'>" +
-	    "<div class='overlay__inner'>" +
-	    "<h2 class='overlay__title'>You’re one message away...</h2>" +
-	    "<form action=''>" +
-	    "<textarea name='message' id='' cols='30' rows='15' placeholder='Write anything here that you think the freelancer will need to know about your project. The more detailed, the better!'></textarea>" +
-	    "<div class='button-container'>" +
-	    "<input class='submit' type='submit' value='Send'>" +
-	    "</div>" +
-	    "</form>" +
-	    "</div>" +
-	    "</section>");
-
-	   	$(".overlay").height(docHeight);
-
-		$(".overlay").click(function(e) {
-		    if (e.target == this) {
-		        $(this).remove();
-		    }
+	    $.get("assets/ajax/form.php", function(data){
+		    $("body").append(data);
+    		$(".overlay").click(function(e) {
+			    if (e.target == this) {
+			        $(this).remove();
+			    }
+			});
 		});
 	});
 });
