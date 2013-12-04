da2afab570b24c67d5b92751dc034db3c7a9d3b6
Josh Johnson <josh@jshjohnson.com>
2013-12-04 17:04:15 +0000

    CSS ribbon on employer pages

--- assets/ajax/apply.php
+++ assets/ajax/apply.php
@@ -1,6 +1,6 @@
 <section class='overlay'>
 	<div class='overlay__inner'> 
-		<h2 class='overlay__title'>You're one message away...</h2>
+		<h2 class='overlay__title'>Show them what you are made of...</h2>
 		<form action=''>
 			<textarea name='message' id='' cols='30' rows='15' placeholder='Write anything here that you think the employer would appreciate knowing about yourself. The more detailed, the better!'></textarea>
 			<div class='button-container'>
