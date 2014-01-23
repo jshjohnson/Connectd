ad40b571af8d74ed10ee1588552dfa615902f761
Josh Johnson <josh@jshjohnson.com>
2014-01-23 12:37:37 +0000

    Config work

--- htdocs/config/config.php
+++ htdocs/config/config.php
@@ -2,12 +2,12 @@
 
 	// // Local environment
 
-	// define("BASE_URL","/Connectd/htdocs/");
-	// define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/Connectd/htdocs/");
+	define("BASE_URL","/Connectd/htdocs/");
+	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/Connectd/htdocs/");
 
-	// Staging environment
-	define("BASE_URL","http://dev.connectd.io");
-	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "domains/dev.connectd.io/current/");
+	// // Staging environment
+	// define("BASE_URL","http://dev.connectd.io");
+	// define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "domains/dev.connectd.io/current/");
 
 	// // Production environment
 	// define("BASE_URL","http://connectd.io");
