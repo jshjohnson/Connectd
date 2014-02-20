f3fdf473ff2ffeaf820d6922c83bb9d5fc7d3018
Josh Johnson <josh@jshjohnson.com>
2014-02-20 23:28:54 +0000

    Move static folder outside htdocs

---
+++ static/assets/ajax/des-skills.php
@@ -0,0 +1,63 @@
+<section class='overlay'>
+	<div class='overlay__inner'> 
+		<h2 class='overlay__title'>Designer skills</h2>
+		<div class="grid--no-marg cf">
+			<table class="skills grid__cell--no-pad module-1-2 float-left">
+				<tbody>
+					<th>User Experience (UX Design)</th>
+					<tr>
+						<td>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, illo, tenetur, blanditiis, quod at similique ut corrupti ratione sequi mollitia debitis suscipit fuga ipsam. Quos nesciunt reiciendis dolore aspernatur? Laudantium?</p>
+						</td>
+					</tr>
+					<th>Interface Design</th>
+					<tr>
+						<td>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, illo, tenetur, blanditiis, quod at similique ut corrupti ratione sequi mollitia debitis suscipit fuga ipsam. Quos nesciunt reiciendis dolore aspernatur? Laudantium?</p>
+						</td>
+					</tr>
+					<th>Graphical Design</th>
+					<tr>
+						<td>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, illo, tenetur, blanditiis, quod at similique ut corrupti ratione sequi mollitia debitis suscipit fuga ipsam. Quos nesciunt reiciendis dolore aspernatur? Laudantium?</p>
+						</td>
+					</tr>
+					<th>Web Design</th>
+					<tr>
+						<td>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, illo, tenetur, blanditiis, quod at similique ut corrupti ratione sequi mollitia debitis suscipit fuga ipsam. Quos nesciunt reiciendis dolore aspernatur? Laudantium?</p>
+						</td>
+					</tr>		      
+				</tbody>
+			</table>
+			<table class="skills grid__cell--no-pad module-1-2 float-right">
+				<tbody>
+					<th>Illustrator</th>
+					<tr>
+						<td>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, illo, tenetur, blanditiis, quod at similique ut corrupti ratione sequi mollitia debitis suscipit fuga ipsam. Quos nesciunt reiciendis dolore aspernatur? Laudantium?</p>
+						</td>
+					</tr>
+					<th>Photoshop</th>
+					<tr>
+						<td>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, illo, tenetur, blanditiis, quod at similique ut corrupti ratione sequi mollitia debitis suscipit fuga ipsam. Quos nesciunt reiciendis dolore aspernatur? Laudantium?</p>
+						</td>
+					</tr>
+					<th>InDesign</th>
+					<tr>
+						<td>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, illo, tenetur, blanditiis, quod at similique ut corrupti ratione sequi mollitia debitis suscipit fuga ipsam. Quos nesciunt reiciendis dolore aspernatur? Laudantium?</p>
+						</td>
+					</tr>
+					<th>Typography</th>
+					<tr>
+						<td>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, illo, tenetur, blanditiis, quod at similique ut corrupti ratione sequi mollitia debitis suscipit fuga ipsam. Quos nesciunt reiciendis dolore aspernatur? Laudantium?</p>
+						</td>
+					</tr>	      
+				</tbody>
+			</table>
+		</div>
+	</div>
+</section>
