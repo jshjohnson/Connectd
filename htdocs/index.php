608a04802c0267673445c8297ef5e255681b2212
Josh Johnson <josh@jshjohnson.com>
2014-02-20 23:30:12 +0000

    Break homepage header/footer into views

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,34 +2,10 @@
 	require_once("config.php"); 
 	require_once(ROOT_PATH . "core/init.php");
 
+	include_once(ROOT_PATH . "views/header-home.php");
 	$general->logged_in_protect();
 
 ?>
-<!doctype html>
-<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"><![endif]-->
-<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
-<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
-<!--[if gt IE 8]><!--><html class="no-js home" lang="en"><!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<title>connectd</title>
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<!--[if lte IE 8]>
-	    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/ie.css" media="screen">
-	    
-	    <script src="<?php echo BASE_URL; ?>assets/js/libs/selectivizr-min.js"></script>
-	<![endif]-->
-	<!--[if gt IE 8]><!-->
-	    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/global.css">
-	<!--<![endif]-->	
-	<script src="<?php echo BASE_URL; ?>assets/js/libs/modernizr-2.5.3.min.js"></script>
-	<script type="text/javascript" src="//use.typekit.net/dxr1afv.js"></script>
-	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-	<script>var baseUrl = '<?= BASE_URL; ?>';</script>
-</head>
-<body>
-	<section id="home" class="site-wrap">
 		<header class="site-intro">
 			<div class="site-intro__wrap">
 				<a href="#register"><h1 class="site-intro__heading">connectd.io</h1></a>
@@ -61,9 +37,4 @@
 				</div>
 			</section>
 		</div>
-	</section>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
-	<script src="assets/js/scripts.min.js"></script>
-</body>
-</html>
+<?php include_once(ROOT_PATH . "views/footer-home.php"); ?>
--- htdocs/views/footer.php
+++ htdocs/views/footer.php
@@ -14,7 +14,7 @@
 		</footer>
 	</div>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo BASE_URL; ?>assets/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
-	<script src="<?php echo BASE_URL; ?>assets/js/scripts.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?= BASE_URL; ?>assets/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+	<script src="<?= BASE_URL; ?>assets/js/scripts.min.js"></script>
 </body>
 </html>
