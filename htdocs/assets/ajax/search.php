0979dd22e6d37d3da1f065e89e8a75259bca2e52
Josh Johnson <josh@jshjohnson.com>
2014-03-10 00:33:43 +0000

    Add variations in size for content overlays // Styling for media object without content

--- htdocs/assets/ajax/search.php
+++ htdocs/assets/ajax/search.php
@@ -1,12 +1,12 @@
 <section class='overlay'>
-	<div class='overlay__inner'> 
+	<div class='overlay__inner overlay__inner--mid'> 
 		<a href="" class="cancel-trigger"><i class="icon--cancel"></i></a>
 		<h2 class='overlay__title'>Find a freelancer</h2>
 		<form action=''>
-			<input type="text" name="firstname" required placeholder="Job title">
-			<input type="text" name="location" required placeholder="Location">
+			<input type="text" name="firstname" placeholder="Job title">
+			<input type="text" name="location" placeholder="Location">
 			<label for="rate">Rate per hour</label>
-			<input type="range" name="rate" required placeholder="Rate" min="20" max="500">
+			<input type="range" name="rate" placeholder="Rate" min="20" max="500">
 			<div class="button-container">
             	<input class="submit" type="submit" value="Search">						
 			</div>
