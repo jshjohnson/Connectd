fb315f386ffad33861309e7bae946cbb991ebfc8
Josh Johnson <josh@jshjohnson.com>
2014-03-20 22:44:51 +0000

    Only show confirmed users on trials

--- htdocs/core/classes/users.php
+++ htdocs/core/classes/users.php
@@ -17,10 +17,8 @@
 			$query->bindParam(1, $email);
 
 			try{
-		 
 				$query->execute();
 				$rows = $query->rowCount();
-		 
 				if($rows > 0){
 					return true;
 				}else{
@@ -92,45 +90,48 @@
 		public function getTrialUsers() {
 
 			$query = $this->db->prepare("
-					SELECT 
-					user_types.user_type,
-					users.user_id,
-					users.firstname, 
-					users.lastname, 
-					users.location, 
-					user_experience.experience,
-					users.portfolio,
-					Count(user_votes.vote_id) 
-					AS CountOfvote_id, 
-					freelancers.jobtitle, 
-					freelancers.priceperhour
-					FROM users 
-					AS voters
-					RIGHT JOIN 
-					((((users LEFT JOIN user_votes 
-					ON users.user_id = user_votes.vote_id) 
-					LEFT JOIN freelancers 
-					ON users.user_id = freelancers.freelancer_id) 
-					LEFT JOIN user_experience
-					ON users.user_id = user_experience.experience_id) 
-					LEFT JOIN user_types
-					ON users.user_id = user_types.user_type_id) 
-					ON voters.user_id = user_votes.vote_id
-					WHERE user_types.user_type != ?
-					GROUP BY
-					users.user_id,
-					users.firstname, 
-					users.lastname, 
-					users.location,
-					user_experience.experience,
-					users.portfolio,
-					user_votes.vote_id,
-					freelancers.jobtitle, 
-					freelancers.priceperhour
-					HAVING CountOfvote_id < ?
+				SELECT 
+				user_types.user_type,
+				users.user_id,
+				users.confirmed,
+				users.firstname, 
+				users.lastname, 
+				users.location, 
+				user_experience.experience,
+				users.portfolio,
+				Count(user_votes.vote_id) 
+				AS CountOfvote_id, 
+				freelancers.jobtitle, 
+				freelancers.priceperhour
+				FROM users 
+				AS voters
+				RIGHT JOIN 
+				((((users LEFT JOIN user_votes 
+				ON users.user_id = user_votes.vote_id) 
+				LEFT JOIN freelancers 
+				ON users.user_id = freelancers.freelancer_id) 
+				LEFT JOIN user_experience
+				ON users.user_id = user_experience.experience_id) 
+				LEFT JOIN user_types
+				ON users.user_id = user_types.user_type_id) 
+				ON voters.user_id = user_votes.vote_id
+				WHERE user_types.user_type != ?
+				AND users.confirmed = ?
+				GROUP BY
+				users.user_id,
+				users.firstname, 
+				users.lastname, 
+				users.location,
+				user_experience.experience,
+				users.portfolio,
+				user_votes.vote_id,
+				freelancers.jobtitle, 
+				freelancers.priceperhour
+				HAVING CountOfvote_id < ?
 			");
 			$query->bindValue(1, 'employer');
-			$query->bindValue(2, 10);
+			$query->bindValue(2, 1);
+			$query->bindValue(3, 10);
 
 			try{
 				$query->execute();
