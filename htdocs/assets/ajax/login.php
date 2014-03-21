e862038a69f13bed5e5d9bbbb92176ce6b03b557
Josh Johnson <josh@jshjohnson.com>
2014-03-21 23:26:37 +0000

    Ajax form url

--- htdocs/assets/ajax/login.php
+++ htdocs/assets/ajax/login.php
@@ -6,7 +6,7 @@
 	<div class='overlay__inner overlay__inner--small'> 
 		<a href="" class="cancel-trigger"><i class="icon--cancel"></i></a>
 		<h2 class='overlay__title'>Log In</h2>
-		<form method="post" action="<?= BASE_URL; ?>login" autocomplete="off">
+		<form method="post" action="<?= BASE_URL; ?>login.php" autocomplete="off">
 			<input type="email" name="email" placeholder="Email" value="<?= $_COOKIE['remember_me']; ?>" autofocus>
 			<input type='password' name='password' placeholder="Password">
 			<fieldset class="checkbox float-left">
