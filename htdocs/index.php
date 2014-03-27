df5f336d05cec9bb88671c99d07f9ba1cd3efb8c
Josh Johnson <josh@jshjohnson.com>
2014-03-27 19:57:06 +0000

    Use index.php for page switching with homepage fallback

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,15 +1,44 @@
 <?php 	
-	require_once("config.php"); 
+	require_once("config.php");  
 	require_once(ROOT_PATH . "core/init.php");
 	
 	$general->errors();
-	$general->loggedInProtect();
+	// $general->loggedOutProtect();
 
-	$pageTitle         = "Connectd";
-	$pageType          = "Home";
-	$section           = "Home";
+	if (isset($_GET['p'])) {
+		$p = $_GET['p'];
+	}else {
+		$p = NULL;
+	}
+
+	switch ($p) {
+		case "about":
+			$page = "about.html";
+			$pageTitle = "About";
+			$pageType = "Page";
+			$section = "Blue";
+			break;
+		case "terms":
+			$page = "terms.html";
+			$pageTitle = "Terms & Conditions";
+			$pageType = "Page";
+			$section = "Blue";
+			break;
+		case "sitemap":
+			$page = "sitemap.html";
+			$pageTitle = "Sitemap";
+			$pageType = "Page";
+			$section = "Blue";
+			break;
+		default:
+			$page = "index.html";
+			$pageTitle = "Connectd";
+			$pageType = "Home";
+			$section = "Home";
+			break;
+	}
 
 	include_once(ROOT_PATH . "includes/header.inc.php");
-	include_once(ROOT_PATH . "views/index.html");
+	include_once(ROOT_PATH . "views/" . $page);
 	include_once(ROOT_PATH . "includes/footer.inc.php");
-?>
+?>  
