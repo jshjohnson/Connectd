6e8ebb7da5fa0983b3fdbba88a4a1c531266e321
Josh Johnson <josh@jshjohnson.com>
2014-04-07 16:23:00 +0100

    Store password values on page if errors

--- htdocs/change-password.php
+++ htdocs/change-password.php
@@ -27,19 +27,24 @@
 		<div class="grid text-center">
 			<div class="grid__cell unit-1-2--bp3 unit-2-3--bp1 content-overlay">
 				<?php
+
+					$currentPassword = $_POST['current_password'];
+					$newPassword = $_POST['new_password'];
+					$repeatNewPassword = $_POST['repeat_new_password'];
+
 			        if(empty($_POST) === false) {
 			           
-			            if(empty($_POST['current_password']) || empty($_POST['new_password']) || empty($_POST['repeat_new_password'])){
+			            if(empty($currentPassword) || empty($newPassword) || empty($repeatNewPassword)){
 			 
 			                $errors[] = 'All fields are required';
 			 
-			            }else if($bcrypt->verify($_POST['current_password'], $user['password']) === true) {
+			            }else if($bcrypt->verify($currentPassword, $user['password']) === true) {
 			 
-			                if (trim($_POST['new_password']) != trim($_POST['repeat_new_password'])) {
+			                if (trim($newPassword) != trim($repeatNewPassword)) {
 			                    $errors[] = 'Your new passwords do not match';
-			                } else if (strlen($_POST['new_password']) < 6) { 
+			                } else if (strlen($newPassword) < 6) { 
 			                    $errors[] = 'Your password must be at least 6 characters';
-			                } else if (strlen($_POST['new_password']) >18){
+			                } else if (strlen($newPassword) >18){
 			                    $errors[] = 'Your password cannot be more than 18 characters long';
 			                } 
 			 
@@ -47,36 +52,35 @@
 			                $errors[] = 'Your current password is incorrect';
 			            }
 			        }
-			 
-			        if (isset($_GET['success']) === true && empty ($_GET['success']) === true ) {
-			    		echo '<p class="message message--success fadeIn">Your password has been changed!</p>';
-			        } else {
-			            if (empty($_POST) === false && empty($errors) === true) {
-			                $users->changePassword($user['user_id'], $_POST['new_password']);
-			                header('Location: change-password.php?success');
-			            } else if (empty ($errors) === false) {               
-			                echo '<p class="message message--error shake">' . implode('</p><p>', $errors) . '</p>';
-			            }
-			        ?>
-						<form action="<?= BASE_URL . "change-password.php"; ?>" method="post" autocomplete="off">
-							<label for="current_password">Current password</label>
-							<input type='password' name='current_password' placeholder="• • • • • • • •" value="">
-							<hr>
-							<fieldset class="cf">
-								<div class="field-1-2 float-left">
-									<label for="new_password">New password</label>
-									<input type='password' name='new_password' placeholder="• • • • • • • •" value="">	
-								</div>		
-								<div class="field-1-2 float-right">
-									<label for="repeat_new_passsword">Repeat password</label>
-									<input type='password' name='repeat_new_password' placeholder="• • • • • • • •" value="">
-								</div>						
-							</fieldset>
-							<div class="btn-container clear">
-								<input class="btn--left btn--green" name="submit" type="submit" value="Change password">
-							</div>
-						</form>
-			    	<?php } ?> 
+
+		            if (empty($_POST) === false && empty($errors) === true) {
+		                $users->changePassword($user['user_id'], $newPassword);
+		                header('Location: change-password.php?success');
+		            } else if (empty ($errors) === false) {               
+		                echo '<p class="message message--error shake">' . implode('</p><p>', $errors) . '</p>';
+		            }
+				?>
+				<?php if (isset($_GET['success']) === true && empty ($_GET['success']) === true ) {
+		    		echo '<p class="message message--success fadeIn">Your password has been changed!</p>';
+		    	} ?>
+				<form action="<?= BASE_URL . "change-password.php"; ?>" method="post" autocomplete="off">
+					<label for="current_password">Current password</label>
+					<input type='password' name='current_password' placeholder="• • • • • • • •" value="<?php if (isset($currentPassword)) { echo htmlspecialchars($currentPassword); } ?>">
+					<hr>
+					<fieldset class="cf">
+						<div class="field-1-2 float-left">
+							<label for="new_password">New password</label>
+							<input type='password' name='new_password' placeholder="• • • • • • • •" value="<?php if (isset($newPassword)) { echo htmlspecialchars($newPassword); } ?>">	
+						</div>		
+						<div class="field-1-2 float-right">
+							<label for="repeat_new_passsword">Repeat password</label>
+							<input type='password' name='repeat_new_password' placeholder="• • • • • • • •" value="<?php if (isset($repeatNewPassword)) { echo htmlspecialchars($repeatNewPassword); } ?>">
+						</div>						
+					</fieldset>
+					<div class="btn-container clear">
+						<input class="btn--left btn--green" name="submit" type="submit" value="Change password">
+					</div>
+				</form>
 			</div>
 		</div>
 	</section>
