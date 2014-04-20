d29226093155aaeab840c5da7e0f64d7dbb0ba07
Josh Johnson <josh@jshjohnson.com>
2014-04-20 13:22:10 +0100

    Use views on search

--- htdocs/search/index.php
+++ htdocs/search/index.php
@@ -22,51 +22,7 @@
 			}
 		}
 	}
+	
+	include(ROOT_PATH . "views/search/search.html");
+	include(ROOT_PATH . "includes/footer.inc.php");
 ?>		
-	<section class="call-to-action call-to-action--search">
-		<div class="container call-to-action__container">
-			<form action="<?= BASE_URL; ?>search/" class="zero-bottom">
-				<input type="search" name="search" placeholder="Search for employers/jobs" value="<?php if (isset($searchTerm)) { echo htmlspecialchars($searchTerm); } ?>" results="5" class="call-to-action__search">
-			</form>
-		</div>
-	</section>
-	<section class="container footer--push">
-		<div class="grid--no-marg text-center cf">
-		<?php if($sessionUserType == "employer") : ?>
-			<article class="dashboard-panel grid__cell module-2-3 module--no-pad">
-				<header class="header--panel header--blue cf">
-					<h3 class="float-left"><a href="<?= BASE_URL; ?>list.php?usertype=designer">Freelancer search</a></h3>
-					<h4 class="float-right icon--search"></h4>
-				</header>
-				<?php if (!empty($allFreelancers) && is_array($allFreelancers)) : ?>
-
-				<?php foreach ($allFreelancers as $freelancer) {
-					$userType = $freelancer['user_type'];
-					include('../views/freelancer/freelancer-list.html');
-				} ?>
-
-				<?php else : ?>
-					<?php include('../views/freelancer/star-empty-list.html'); ?>
-				<?php endif;?>
-			</article>
-		<?php else : ?>
-			<article class="dashboard-panel grid__cell module-2-3 module--no-pad">
-				<header class="header--panel header--green cf">
-					<h3 class="float-left"><a href="<?= BASE_URL; ?>list.php?usertype=designer">Job search</a></h3>
-					<h4 class="float-right icon--search"></h4>
-				</header>
-				<?php if (!empty($allJobs) && is_array($allJobs)) : ?>
-
-				<?php foreach ($allJobs as $job) {
-					$userType = $freelancer['user_type'];
-					include('../views/job/job-list.html');
-				} ?>
-
-				<?php else : ?>
-					<?php include('../views/job/job-empty-list.html'); ?>
-				<?php endif;?>
-			</article>
-		<?php endif; ?>
-		</div>
-	</section>
-<?php include(ROOT_PATH . "includes/footer.inc.php"); ?>
