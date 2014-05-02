b0d4aa4c3894d4a10efdb8087f26fb92e73c7385
Josh Johnson <josh@jshjohnson.com>
2014-05-02 12:32:02 +0100

    Move controller code in confirm-recover page into controller

--- htdocs/settings/confirm-recover.php
+++ htdocs/settings/confirm-recover.php
@@ -1,11 +1,22 @@
 <?php
-	require_once("../config.php"); 
+	require("../config.php"); 
 	require(ROOT_PATH . "core/init.php");
 
 	$pageTitle = "Recover password";
 	$pageType = "Page";
 	$section = "Blue";
 
+	if (isset($_POST['email']) === true && empty($_POST['email']) === false) {
+		$email = $_POST['email'];
+		if ($users->emailExists($email) === true){
+			$users->confirmRecover($email);
+			header('Location:' . BASE_URL . 'settings/recover-password/?success');
+			exit();
+		} else {
+			$errors[] = 'Sorry, that email doesn\'t exist.';
+		}
+	}
+	
 	include(ROOT_PATH . "includes/header.inc.php");
 	include(ROOT_PATH . "views/settings/confirm-recover.html");
 	include(ROOT_PATH . "includes/footer.inc.php");
