d8d760b6bbf55799947c2136a157cfc43b09c681
Josh Johnson <josh@jshjohnson.com>
2014-05-02 18:49:33 +0100

    Add job to types of lists

--- htdocs/list.php
+++ htdocs/list.php
@@ -6,16 +6,16 @@
 	
 	try {
 
-		if (isset($_GET['usertype'])) {
-			$userType = $_GET['usertype'];
+		if (isset($_GET['type'])) {
+			$type = $_GET['type'];
 		}else {
 			throw new Exception('A user type was not set.');
-			$userType = NULL;
+			$type = NULL;
 		}
 
 		$status = $_GET["status"];
 
-		switch ($userType) {
+		switch ($type) {
 			case "designer":
 				$section = "Designer";
 				$users->loggedOutProtect();
@@ -31,6 +31,11 @@
 				$users->loggedOutProtect();
 				$pageTitle = "Employer list";
 				break;
+			case "jobs":
+				$section = "Jobs";
+				$users->loggedOutProtect();
+				$pageTitle = "Job list";
+				break;
 			default:
 				$template = "index/index.html";
 				$pageTitle = "Connectd";
@@ -40,16 +45,21 @@
 				break;
 		}
 
-		if($userType == "developer" || $userType == "designer") {
+		if($type == "developer" || $type == "designer") {
 			$freelancers = $freelancers->getFreelancersAll($userType);	
 			$freelancer_id = $_GET["id"];	
-		} else if ($userType == "employer") {
+			$template = "list-user.html";
+		} else if ($type == "employer") {
 			$employers = $employers->getEmployersAll($userType);
 			$employer_id = $_GET["id"];
+			$template = "list-user.html";
+		} else if ($type == "jobs") {
+			$allJobs = $jobs->getJobsAll();
+			$template = "list-job.html";
 		}
 
 		include(ROOT_PATH . "includes/header.inc.php");
-		include(ROOT_PATH . "views/page/list.html");
+		include(ROOT_PATH . "views/page/" . $template);
 		include(ROOT_PATH . "includes/footer.inc.php");
 
 	}catch(Exception $e) {
