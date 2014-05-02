612e0b78c3025fbffecc3b38838dcb7ee846791f
Josh Johnson <josh@jshjohnson.com>
2014-05-02 12:31:18 +0100

    Move recover password template into a view

--- htdocs/recover.php
+++ htdocs/recover.php
@@ -5,49 +5,28 @@
 	$pageTitle = "Recover password";
 	$pageType = "Page";
 	$section = "Blue";
-	include(ROOT_PATH . "includes/header.inc.php");
-?>
-	<section>
-		<div class="section-heading color-blue">
-			<div class="container">
-				<div class="grid text-center">
-					<div class="grid__cell unit-1-1--bp2 unit-3-4--bp1">
-						<blockquote class="intro-quote text-center">
-							Recover Password
-						</blockquote>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<section class="footer--push color-navy">
-		<div class="grid text-center">
-			<div class="grid__cell unit-1-2--bp3 unit-2-3--bp1 content-overlay">
-    		<?php if (isset($_GET['success']) === true && empty ($_GET['success']) === true) { ?>
-            	<p class="message message--success fadeIn">Thank you, we've sent you a randomly generated password in your email.</p>
-            <?php } else if (isset ($_GET['email'], $_GET['generated_string']) === true) {
+
+	if (isset($_GET['email'], $_GET['generated_string']) === true) {
             
-					$email = trim($_GET['email']);
-					$string = trim($_GET['generated_string']);	
+		$email = trim($_GET['email']);
+		$generatedString = trim($_GET['generated_string']);	
 
-					if ($users->emailExists($email) === false || $users->recover($email, $string) === false) {
-						$errors[] = 'Sorry, something went wrong and we couldn\'t recover your password.';
-					}
+		if ($users->emailExists($email) === false) {
+			$errors[] = 'Sorry, that email does not exist on our system.';
+		} else if($users->recover($email, $generatedString) === false) {
+			$errors[] = 'Sorry, we couldn\'t recover your password';
+		}
 
-					if (empty($errors) === false) {    		
-						echo '<p class="message message--error shake">' . implode('</p><p>', $errors) . '</p>';
-					} else {
-						#redirect the user to recover.php?success if recover() function does not return false.
-						header('Location: recover.php?success');
-						exit();
-					}
-
-					} else {
-						header('Location: index.php'); // If the required parameters are not there in the url. redirect to index.php
-						exit();
-					}
-		       ?>
-			</div>
-		</div>
-	</section>
-<?php include(ROOT_PATH . "includes/footer.inc.php"); ?>
+		if (!empty($errors) === false) {  
+			header('Location: '. BASE_URL . 'recover.php?success');
+			exit();
+		}else {
+			header('Location: '. BASE_URL . 'settings/recover-password/');
+			exit();
+		}
+	} 
+	
+	include(ROOT_PATH . "includes/header.inc.php");
+	include(ROOT_PATH . "views/settings/recover.html");
+	include(ROOT_PATH . "includes/footer.inc.php"); 
+?>
