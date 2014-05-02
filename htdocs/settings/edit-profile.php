139e4b9f6a045e8bfe52b0556b37a8da528c7d6d
Josh Johnson <josh@jshjohnson.com>
2014-05-02 18:50:05 +0100

    Edit profile -> account settings // Duplicate of profile page for edit profile template

--- htdocs/settings/edit-profile.php
+++ htdocs/settings/edit-profile.php
@@ -1,95 +1,86 @@
-<?php
-	require("../config.php"); 
-	require(ROOT_PATH . "core/init.php");
-
-	$pageTitle = "Edit Profile";
-	$pageType = "Page";
-	$section = "Blue";
-
-	if(empty($_POST) === false) {
-
-		$firstName = trim($_POST['firstname']);
-		$lastName = trim($_POST['lastname']);
-		$email = trim($_POST['email']);
-		$portfolio = trim($_POST['portfolio']);
-		$bio = trim($_POST['bio']);
+<?php 
+	require_once("../config.php"); 
+	require(ROOT_PATH . "core/init.php"); 
 	
-		if (isset($firstName) && !empty($firstName)){ 
-			if (ctype_alpha($firstName) === false) {
-				$errors[] = 'Please enter your first name with only letters';
-			}	
-		} else {
-			$errors[] = 'Please enter your first name';
-		}
+	$users->loggedOutProtect();
 
-		if (isset($lastName) && !empty ($lastName)){
-			if (ctype_alpha($lastName) === false) {
-				$errors[] = 'Please enter your last name with only letters';
-			}	
-		} else {
-			$errors[] = 'Please enter your last name';
-		}
-
-		if(empty($email)){
-			$errors[] = "Please enter your email"; 
-		}else if($email != $sessionEmail) {
-			if (!$mail->ValidateAddress($email)){
-				$errors[] = "You must specify a valid email address.";
-			}else if ($users->emailExists($email) === true) {
-			    $errors[] = "Email already taken. Please try again.";
-			}
-		}
-
-		if (empty($bio)){
-			$errors[] = 'Please enter a bio';
-		}else if(strlen($bio)<25) {
-			$errors[] = "You're not going to sell yourself without a decent bio!";
+	try {
+		if (isset($_GET["id"])) {
+			$pageID = intval($_GET["id"]);
 		}
 		
-		if(empty($portfolio)){
-			$errors[] = "Please specify a valid portfolio"; 
+		if (isset($_GET['usertype'])) {
+			$userType = $_GET['usertype'];
+		}else {
+			throw new Exception('A user type was not set.');
+			$userType = NULL;
 		}
 
-		if (isset($_FILES['avatar']) && !empty($_FILES['avatar']['name'])) {	
-			$name 			= $_FILES['avatar']['name'];
-			$tmp_name 		= $_FILES['avatar']['tmp_name'];
-			$allowed_ext 	= array('jpg', 'jpeg', 'png', 'gif', 'svg');
-			$a 				= explode('.', $name);
-			$file_ext 		= strtolower(end($a)); unset($a);
-			$file_size 		= $_FILES['avatar']['size'];
-			$path 			= "assets/avatars";
-			
-			if (in_array($file_ext, $allowed_ext) === false) {
-				$errors[] = 'Image file type not allowed';	
-			}	
-			if ($file_size > 2097152) {
-				$errors[] = 'File size must be under 2mb';
-			}
-			
-		} else {
-			$newpath = $sessionAvatar;
+		$userExists = $users->userType($pageID);
+		$profileUserType = $userExists['user_type'];
+		$profileUserInfo = $users->fetchInfo("granted_access", "user_id", $pageID);
+
+		// If id isn't in the URL OR the id is is not an integer and not greater than or equal to 1, throw error
+		if (!filter_var($pageID, FILTER_VALIDATE_INT, array('min_range' => 1))) {
+			throw new Exception('Uh oh! An invalid page ID was provided to this page.');
+		} else if($profileUserType != $userType){
+			throw new Exception('Uh oh! This user has does not exist.');
+		} else if($profileUserInfo == 0) {
+			throw new Exception('Uh oh! That user has not been voted into the Connectd community yet.');
 		}
 
-		if(empty($errors) === true) {
-			
-			if (isset($_FILES['avatar']) && !empty($_FILES['avatar']['name'])) {
-				$newpath = $forms->fileNewPath($path, $name);
-				move_uploaded_file($tmp_name, $newpath);
-			}
-					
-			$firstName 	= htmlentities(trim($_POST['firstname']));
-			$lastName = htmlentities(trim($_POST['lastname']));	
-			$bio = htmlentities(trim($_POST['bio']));
-			$imageLocation = htmlentities(trim($newpath));
-			
-			$users->updateUser($firstName, $lastName, $portfolio, $email, $bio, $imageLocation, $sessionUserID);
-			header('Location: ' . BASE_URL . 'settings/edit-profile/?success');
-			exit();
-		
-		}	
-    }
+		$status = $_GET["status"];
+		$starredBy = $_SESSION['user_id'];
 
-	include(ROOT_PATH . "includes/header.inc.php");
-	include(ROOT_PATH . "views/settings/edit-profile.html");
-	include(ROOT_PATH . "includes/footer.inc.php");
+		switch ($userType) {
+			case "designer":
+				$section = "Designer";
+				$pageType = "Custom";
+				$section = "Designer";
+				$users->loggedOutProtect();
+				break;
+			case "developer":
+				$section = "Developer";
+				$pageType = "Custom";
+				$section = "Developer";
+				$users->loggedOutProtect();
+				break;
+			case "employer":
+				$section = "Employer";
+				$pageType = "Custom";
+				$section = "Employer";
+				$jobs = $jobs->getEmployerJobs($pageID);
+				$users->loggedOutProtect();
+				break;
+			default:
+				$template = "index/index.html";
+				$pageTitle = "Connectd";
+				$pageType = "Home";
+				$section = "Home";
+				$users->loggedInProtect();
+				break;
+		}
+
+		if($userType == "developer" || $userType == "designer") {
+			$user = $freelancers->getFreelancersSingle($pageID, $userType);		
+			$pageTitle  = ucwords($user['firstname']) . ' ' . ucwords($user['lastname']) . ' :: ' . $user['jobtitle'];
+			$_SESSION["userFirstName"] = $user['firstname'];
+			$_SESSION["userEmail"] = $user['email'];
+			$template = "freelancer/freelancer-profile.html";
+		} else if ($userType == "employer") {
+			$user = $employers->getEmployersSingle($pageID);
+			$employerName = ucwords($user['employer_name']);	
+			$pageTitle  = $employerName . ' :: ' . $user['employer_type'];
+			$template = "employer/employer-profile.html";
+		}
+
+		include(ROOT_PATH . "includes/header.inc.php");
+		include(ROOT_PATH . "views/" . $template);
+		include(ROOT_PATH . "includes/footer.inc.php");
+
+	}catch(Exception $e) {
+		$users = new Users($db);
+		$debug = new Errors();
+		$debug->errorView($users, $e);	
+	}
 ?>
