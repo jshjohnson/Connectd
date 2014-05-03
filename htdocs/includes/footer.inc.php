93024f941e20c533e29f0a90c10bb7233cc68999
Josh Johnson <josh@jshjohnson.com>
2014-05-03 13:38:05 +0100

    Textboxlist plugin working on developer edit profile view

--- htdocs/includes/footer.inc.php
+++ htdocs/includes/footer.inc.php
@@ -24,5 +24,19 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="<?= BASE_URL; ?>assets/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
 	<script src="<?= BASE_URL; ?>assets/js/scripts.min.js"></script>
+	<script src="<?= BASE_URL; ?>assets/js/libs/mootools-1.2.1-core-yc.js" type="text/javascript" charset="utf-8"></script>		
+	<!-- required for TextboxList -->
+	<script src="<?= BASE_URL; ?>assets/js/libs/GrowingInput.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?= BASE_URL; ?>assets/js/libs/TextboxList.js" type="text/javascript" charset="utf-8"></script>		
+	<script src="<?= BASE_URL; ?>assets/js/libs/TextboxList.Autocomplete.js" type="text/javascript" charset="utf-8"></script>
+	<!-- required for TextboxList.Autocomplete if method set to 'binary' -->
+	<script src="<?= BASE_URL; ?>assets/js/libs/TextboxList.Autocomplete.Binary.js" type="text/javascript" charset="utf-8"></script>		
+	<script>
+		window.addEvent('load', function(){
+			// With custom adding keys 
+			var t2 = new TextboxList('form_tags_input_2', {bitsOptions:{editable:{addKeys: 188}}});
+			t2.add('HTML5').add('CSS3');
+		});
+	</script>
 </body>
 </html>
--- htdocs/settings/edit-profile.php
+++ htdocs/settings/edit-profile.php
@@ -1,36 +1,13 @@
 <?php 
-	require_once("../config.php"); 
+	require("../config.php"); 
 	require(ROOT_PATH . "core/init.php"); 
 	
 	$users->loggedOutProtect();
 
 	try {
-		if (isset($_GET["id"])) {
-			$pageID = intval($_GET["id"]);
-		}
-		
-		if (isset($_GET['usertype'])) {
-			$userType = $_GET['usertype'];
-		}else {
-			throw new Exception('A user type was not set.');
-			$userType = NULL;
-		}
 
-		$userExists = $users->userType($pageID);
-		$profileUserType = $userExists['user_type'];
-		$profileUserInfo = $users->fetchInfo("granted_access", "user_id", $pageID);
-
-		// If id isn't in the URL OR the id is is not an integer and not greater than or equal to 1, throw error
-		if (!filter_var($pageID, FILTER_VALIDATE_INT, array('min_range' => 1))) {
-			throw new Exception('Uh oh! An invalid page ID was provided to this page.');
-		} else if($profileUserType != $userType){
-			throw new Exception('Uh oh! This user has does not exist.');
-		} else if($profileUserInfo == 0) {
-			throw new Exception('Uh oh! That user has not been voted into the Connectd community yet.');
-		}
-
-		$status = $_GET["status"];
-		$starredBy = $_SESSION['user_id'];
+		$userType = $sessionUserType;
+		$pageID = $sessionUserID;
 
 		switch ($userType) {
 			case "designer":
@@ -45,13 +22,6 @@
 				$section = "Developer";
 				$users->loggedOutProtect();
 				break;
-			case "employer":
-				$section = "Employer";
-				$pageType = "Custom";
-				$section = "Employer";
-				$jobs = $jobs->getEmployerJobs($pageID);
-				$users->loggedOutProtect();
-				break;
 			default:
 				$template = "index/index.html";
 				$pageTitle = "Connectd";
@@ -62,16 +32,9 @@
 		}
 
 		if($userType == "developer" || $userType == "designer") {
-			$user = $freelancers->getFreelancersSingle($pageID, $userType);		
+			$user = $freelancers->getFreelancersSingle($pageID, $userType);
 			$pageTitle  = ucwords($user['firstname']) . ' ' . ucwords($user['lastname']) . ' :: ' . $user['jobtitle'];
-			$_SESSION["userFirstName"] = $user['firstname'];
-			$_SESSION["userEmail"] = $user['email'];
-			$template = "freelancer/freelancer-profile.html";
-		} else if ($userType == "employer") {
-			$user = $employers->getEmployersSingle($pageID);
-			$employerName = ucwords($user['employer_name']);	
-			$pageTitle  = $employerName . ' :: ' . $user['employer_type'];
-			$template = "employer/employer-profile.html";
+			$template = "settings/edit-profile.html";
 		}
 
 		include(ROOT_PATH . "includes/header.inc.php");
