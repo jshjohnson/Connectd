d3869ab517febe5e94802f86dd83aed2be8f01aa
Josh Johnson <josh@jshjohnson.com>
2014-05-03 16:32:52 +0100

    Edit profile structuring + logic

--- htdocs/settings/edit-profile.php
+++ htdocs/settings/edit-profile.php
@@ -8,16 +8,15 @@
 
 		$userType = $sessionUserType;
 		$pageID = $sessionUserID;
+		$pageTitle = "Edit Profile";
 
 		switch ($userType) {
 			case "designer":
-				$section = "Designer";
 				$pageType = "Custom";
 				$section = "Designer";
 				$users->loggedOutProtect();
 				break;
 			case "developer":
-				$section = "Developer";
 				$pageType = "Custom";
 				$section = "Developer";
 				$users->loggedOutProtect();
@@ -35,6 +34,68 @@
 			$user = $freelancers->getFreelancersSingle($pageID, $userType);
 			$pageTitle  = ucwords($user['firstname']) . ' ' . ucwords($user['lastname']) . ' :: ' . $user['jobtitle'];
 			$template = "settings/edit-profile.html";
+		}
+
+
+		if(empty($_POST) === false) {
+
+			$skills = explode(',', $_POST['skills']);
+
+			$testimonial = trim($_POST['testimonial']);
+			$testimonialSource = trim($_POST['testimonial-source']);
+
+			// If Testimonial is not empty but source is
+			if(!empty($testimonial) && empty($testimonialSource)) {
+				$errors[] = "You must specify the source of your testimonial";
+			}
+
+			// If less than 3 skills are set
+			if(is_array($skills) && !empty($skills) && count($skills) < 3) {
+				$errors[] = "You must specify at least 3 skills";
+			}
+
+			if(isset($_FILES['portfolio-piece']) && !empty($_FILES['portfolio-piece']['name'])) {
+				// Number of uploaded files
+				$num_files = count($_FILES['portfolio-piece']['name']);
+
+				/** loop through the array of files ***/
+				for($x =0; $x< $num_files;$x++){
+
+					$image = $_FILES['portfolio-piece']['name'][$x];
+					$tmp_name = $_FILES['portfolio-piece']['tmp_name'][$x];
+					$allowed_ext = array('jpg', 'jpeg', 'png', 'gif', 'svg');
+					$a = explode('.', $image);
+					$file_ext = strtolower(end($a)); unset($a);
+					$file_size = $_FILES['portfolio-piece']['size'][$x];
+					$path = "assets/portfolio-pieces";
+
+					if (in_array($file_ext, $allowed_ext) === false) {
+						$errors[] = 'Image file type not allowed';	
+					}	
+					if ($file_size > 2097152) {
+						$errors[] = 'File size must be under 2mb';
+					}
+   
+					if(!is_uploaded_file($_FILES['portfolio-piece']['tmp_name'][$x])){
+					    $errors[] = $image.' No file uploaded';
+					}
+				}
+			}
+
+
+			if(empty($errors) === true) {
+
+				if (isset($_FILES['portfolio-piece']) && !empty($_FILES['portfolio-piece']['name'])) {
+					$newpath = $forms->fileNewPath($path, $image);
+					move_uploaded_file($tmp_name, $newpath);
+				}
+
+				$imageLocation = htmlentities(trim($newpath));
+
+				$user->updateProfile($skills, $imageLocation, $testimonial, $testimonialSource); 
+				header('Location: ' . BASE_URL . 'settings/edit-profile/?success');
+				exit();
+			}
 		}
 
 		include(ROOT_PATH . "includes/header.inc.php");
