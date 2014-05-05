c4c3a6c13e89c979b70c75cfaf6ba972a340d1f7
Josh Johnson <josh@jshjohnson.com>
2014-05-05 13:11:29 +0100

    Use ROOT_PATH constant for grabbing email templates

--- htdocs/core/classes/emails.class.php
+++ htdocs/core/classes/emails.class.php
@@ -6,7 +6,6 @@
 		}
 
 		public function sendEmail($email, $subject, $body) {
-
 			try {
 				$this->mail->IsSMTP(); 
 				$this->mail->Username           = "hello@connectd.io"; 
@@ -16,18 +15,12 @@
 				$this->mail->Host               = "smtp.gmail.com";  
 				$this->mail->Port               = 587; 
 				$this->mail->addAddress($email);  
-
 				$this->mail->From               = 'hello@connectd.io';
 				$this->mail->FromName           = 'Connectd.io';
-				
 				$this->mail->isHTML(true); 
-
 				$this->mail->MsgHTML($body);
-
 				$this->mail->Subject = $subject;
-
 				$this->mail->Send();
-
 			}catch(phpmailerException $e) {
 					$users = new Users($db);
 					$debug = new Errors();
@@ -42,7 +35,8 @@
 	    public function sendVoteEmail($firstName, $email, $votes) {
 	    	try {
 		    	$subject = "You just got a vote on Connectd Trials!";
-		    	$body = file_get_contents('../assets/email-templates/vote-added.html');
+		    	$body = file_get_contents(ROOT_PATH . 'assets/email-templates/vote-added.html');
+		    	$body = str_replace('{{subject}}', $subject, $body);
 		    	$body = str_replace('{{name}}', $firstName, $body);
 		    	$body = str_replace('{{votes}}', $votes['CountOfvote_id'], $body);
 		    	$body = str_replace('{{url}}', BASE_URL, $body);
@@ -57,7 +51,8 @@
 	    public function sendTrialEndedEmail($firstName, $email) {
 	    	try {
 		    	$subject = "Congratulations - You have been granted access to the Connectd Community.";
-		    	$body = file_get_contents('../assets/email-templates/trial-ended.html');
+		    	$body = file_get_contents(ROOT_PATH . 'assets/email-templates/trial-ended.html');
+		    	$body = str_replace('{{subject}}', $subject, $body);
 		    	$body = str_replace('{{name}}', $firstName, $body);
 		    	$body = str_replace('{{url}}', BASE_URL, $body);
 		    	$this->sendEmail($email, $subject, $body);
@@ -71,7 +66,8 @@
 		public function sendRecoverPasswordEmail($firstName, $email, $generatedString) {
 			try {
 		    	$subject = "Reset password -  Connectd.io";
-		    	$body = file_get_contents('../assets/email-templates/reset-password.html');
+		    	$body = file_get_contents(ROOT_PATH . 'assets/email-templates/reset-password.html');
+		    	$body = str_replace('{{subject}}', $subject, $body);
 		    	$body = str_replace('{{name}}', $firstName, $body);
 		    	$body = str_replace('{{email}}', $email, $body);
 		    	$body = str_replace('{{string}}', $generatedString, $body);
@@ -88,7 +84,8 @@
 	    public function sendNewPasswordEmail($firstName, $email, $generatedPassword) {
 			try {
 		    	$subject = "Your new password -  Connectd.io";
-		    	$body = file_get_contents('assets/email-templates/new-password.html');
+		    	$body = file_get_contents(ROOT_PATH . 'assets/email-templates/new-password.html');
+		    	$body = str_replace('{{subject}}', $subject, $body);
 		    	$body = str_replace('{{name}}', $firstName, $body);
 		    	$body = str_replace('{{newpassword}}', $generatedPassword, $body);
 		    	$body = str_replace('{{url}}', BASE_URL, $body);
@@ -104,7 +101,8 @@
 		public function sendConfirmationEmail($firstName, $email, $emailCode) {
 			try {
 		    	$subject = "Activate your new Connectd account -  Connectd.io";
-		    	$body = file_get_contents('../assets/email-templates/confirmation.html');
+		    	$body = file_get_contents(ROOT_PATH . 'assets/email-templates/confirmation.html');
+		    	$body = str_replace('{{subject}}', $subject, $body);
 		    	$body = str_replace('{{name}}', $firstName, $body);
 		    	$body = str_replace('{{email}}', $email, $body);
 		    	$body = str_replace('{{code}}', $emailCode, $body);
@@ -120,7 +118,8 @@
 	    public function sendMessageEmail($firstName, $email, $message, $sentBy) {
 			try {
 		    	$subject = "You just got a message - Connectd.io";
-		    	$body = file_get_contents('assets/email-templates/message.html');
+		    	$body = file_get_contents(ROOT_PATH . 'assets/email-templates/message.html');
+		    	$body = str_replace('{{subject}}', $subject, $body);
 		    	$body = str_replace('{{name}}', $firstName, $body);
 		    	$body = str_replace('{{sentBy}}', $sentBy, $body);
 		    	$body = str_replace('{{message}}', $message, $body);
