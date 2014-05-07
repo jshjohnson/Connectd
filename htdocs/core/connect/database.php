3279e192d80af01c60b0bf0111cb365fb0ba5549
Josh Johnson <josh@jshjohnson.com>
2014-05-07 19:01:00 +0100

    Use PDO persistent connection to improve performance

--- htdocs/core/connect/database.php
+++ htdocs/core/connect/database.php
@@ -9,7 +9,7 @@
 
 	try {
 		#connecting to the database by supplying required parameters
-		$db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password']);
+		$db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password'], array(PDO::ATTR_PERSISTENT => true));
 
 		#Setting the error mode of our db object, which is very important for debugging.
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
