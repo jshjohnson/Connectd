bd3861d3e7a5b1ef9fb9b00a8d05e4cc8bd5354d
Josh Johnson <josh@jshjohnson.com>
2014-05-07 14:47:50 +0100

    Compressed CSS

--- htdocs/config/config.rb
+++ htdocs/config/config.rb
@@ -8,7 +8,7 @@ images_dir = "assets/images"
 javascripts_dir = "assets/js"
 fonts_dir = "assets/fonts"
 
-output_style = :expanded
+output_style = :compressed
 environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
