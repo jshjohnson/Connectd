4d1dac96dfdc1de31c8a74adfb758c4cdab1b059
Josh Johnson <josh@jshjohnson.com>
2014-05-08 12:38:39 +0100

    Put delete job in try catch block testing for id

--- htdocs/jobs/delete-job.php
+++ htdocs/jobs/delete-job.php
@@ -2,11 +2,20 @@
 	require("../config.php"); 
 	require(ROOT_PATH . "core/init.php");
 
-	if (isset($_GET['job_id'])) {
-		$jobID = $_GET['job_id'];
-	}
+	try {
+		// If id isn't in the URL OR the id is is not an integer and not greater than or equal to 1, throw error
+		if (!isset($_GET["job_id"]) || !filter_var($_GET['job_id'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
+			throw new Exception('An invalid page ID was provided to this page.');
+		}
+
+		$jobID = intval($_GET['job_id']);
 	
-	if($jobID != '' && is_numeric($jobID)) {
-		$jobs->deleteJob($jobID);
+		if($jobID != '' && is_numeric($jobID)) {
+			$jobs->deleteJob($jobID);
+		}
+	}catch(Exception $e) {
+		$users = new Users($db);
+		$debug = new Errors();
+		$debug->errorView($users, $e);	
 	}
 ?>
