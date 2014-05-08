c774cbece4ee00bcd4bf701bc443b14c606ecbfe
Josh Johnson <josh@jshjohnson.com>
2014-05-08 11:40:27 +0100

    Added function to grab twitter anchors and links from bios

--- htdocs/core/classes/urls.class.php
+++ htdocs/core/classes/urls.class.php
@@ -21,5 +21,25 @@
 	        }
 	        return $out;
 	    }
+
+	   public function twitterLinks($text) {
+			// convert URLs into links
+			$text = preg_replace(
+				"#(https?://([-a-z0-9]+\.)+[a-z]{2,5}([/?][-a-z0-9!\#()/?&+]*)?)#i", "<a href='$1' target='_blank'>$1</a>",
+				$text);
+			// convert protocol-less URLs into links
+			$text = preg_replace(
+				"#(?!https?://|<a[^>]+>)(^|\s)(([-a-z0-9]+\.)+[a-z]{2,5}([/?][-a-z0-9!\#()/?&+.]*)?)\b#i", "$1<a href='http://$2'>$2</a>",
+				$text);
+			// convert @mentions into follow links
+			$text = preg_replace(
+				"#(?!https?://|<a[^>]+>)(^|\s)(@([_a-z0-9\-]+))#i", "$1<a href=\"http://twitter.com/$3\" title=\"Follow $3\" target=\"_blank\">@$3</a>",
+				$text);
+			// convert #hashtags into tag search links
+			$text = preg_replace(
+				"#(?!https?://|<a[^>]+>)(^|\s)(\#([_a-z0-9\-]+))#i", "$1<a href='http://twitter.com/search?q=%23$3' title='Search tag: $3' target='_blank'>#$3</a>",
+				$text);
+			return $text;
+		}
 		
 	}
