fae47b6da8acdcfc7cf2bc9e28a8bd636303ee74
Josh Johnson <josh@jshjohnson.com>
2014-05-10 10:55:58 +0100

    Add current class to current page

--- htdocs/includes/page-nav.inc.php
+++ htdocs/includes/page-nav.inc.php
@@ -12,20 +12,19 @@
 					<a href="#nav" class="nav-toggle nav-toggle--close icon--cancel" id="nav-close-btn"></a>
 					<ul>
 					<?php if ($users->loggedIn() === true) : ?>
-						<li><a href="<?= BASE_URL; ?>dashboard/">Dashboard</a></li>
-						<li><a href="<?= BASE_URL; ?>trials/">Trials</a></li>
-						<li><a href="<?= BASE_URL . $sessionUserType . "/profile/" . $sessionUser['user_id'] . "/"?>">View Profile</a></li>
+						<li><a <?php if (strpos($_SERVER['PHP_SELF'], 'dashboard/index.php')) echo 'class="current"';?> href="<?= BASE_URL; ?>dashboard/">Dashboard</a></li>
+						<li><a <?php if (strpos($_SERVER['PHP_SELF'], 'trials/index.php')) echo 'class="current"';?> href="<?= BASE_URL; ?>trials/">Trials</a></li>
+						<li><a <?php if (strpos($_SERVER['PHP_SELF'], 'profile.php')) echo 'class="current"';?> href="<?= BASE_URL . $sessionUserType . "/profile/" . $sessionUser['user_id'] . "/"?>">View Profile</a></li>
 						<?php if($sessionUserType != "employer") : ?>
-						<li><a href="<?= BASE_URL . "jobs/list/"; ?>">Job Board</a></li>
-						<li><a href="<?= BASE_URL . "settings/edit-profile/"; ?>">Edit Profile</a></li>
+						<li><a <?php if (strpos($_SERVER['PHP_SELF'], 'list.php')) echo 'class="current"';?> href="<?= BASE_URL . "jobs/list/"; ?>">Job Board</a></li>
+						<li><a <?php if (strpos($_SERVER['PHP_SELF'], 'settings/edit-profile.php')) echo 'class="current"';?> href="<?= BASE_URL . "settings/edit-profile/"; ?>">Edit Profile</a></li>
 						<?php endif; ?>
-						<li><a href="<?= BASE_URL . "settings/account-settings/"; ?>">Account Settings</a></li>
-						<li><a href="<?= BASE_URL . "settings/change-password/"; ?>">Change Password</a></li>
+						<li><a <?php if (strpos($_SERVER['PHP_SELF'], 'settings/account-settings.php')) echo 'class="current"';?> href="<?= BASE_URL . "settings/account-settings/"; ?>">Account Settings</a></li>
+						<li><a <?php if (strpos($_SERVER['PHP_SELF'], 'settings/change-password.php')) echo 'class="current"';?> href="<?= BASE_URL . "settings/change-password/"; ?>">Change Password</a></li>
 						<li><a href="<?= BASE_URL; ?>logout/">Log out</a></li>
 					<?php else : ?>
 						<li><a href="<?= BASE_URL; ?>">Home</a></li>
 						<li><a href="<?= BASE_URL; ?>index.php#register">Register</a></li>
-						<li><a href="<?= BASE_URL; ?>about/">About</a></li>
 					<?php endif; ?>
 					</ul>
 				</nav>
