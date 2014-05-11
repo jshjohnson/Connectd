e74968414a452870442477f55e91b8ab5a31902b
Josh Johnson <josh@jshjohnson.com>
2014-05-11 18:49:08 +0100

    Test if previous URL has "/job/" within it - if so send job application email

--- htdocs/message.php
+++ htdocs/message.php
@@ -7,11 +7,24 @@
 
 	$userFirstName = $_SESSION["userFirstName"];
 	$userEmail = $_SESSION["userEmail"];
-	$message = preg_replace('/\s*$^\s*/m', "\n", $_POST['message']);
-	$sentByName = $sessionUsername;
-	$sentByEmail = $sessionEmail;
 
-	$emails->sendMessageEmail($userFirstName, $userEmail, $message, $sentByName, $sentByEmail);
+	$userID = $users->fetchInfo("user_id", "email", $userEmail);
+
+	$getUserType = $users->userType($userID);
+	$userUserType = $getUserType['user_type'];
+
+	$jobID = $_SESSION["job_id"];
+
+	$message = preg_replace('/\s*$^\s*/m', "\n", $_POST['message']);
+	$sessionProfileURL = $sessionUserType . "/profile/" . $sessionUserID . "/";;
+
+	if(!empty($jobID) && strpos($_SERVER['HTTP_REFERER'],'/job/')) {
+		$jobs->insertJobApplication($sessionUserID, $jobID);
+		$emails->sendJobApplicationEmail($userFirstName, $userEmail, $message, $sessionUsername, $sessionEmail, $sessionProfileURL);
+	} else {
+		$emails->sendMessageEmail($userFirstName, $userEmail, $message, $sessionUsername, $sessionEmail, $sessionProfileURL);
+	}
+	
 	unset($userFirstName, $userEmail);
 	header('Location: ' . $_SERVER['HTTP_REFERER'] . "?sent/");
 	
